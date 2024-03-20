58f2c47cc8fbe0cffb33e2b13391dd0b54928aa9
Andrick Buitrago <sterraquio@hotmail.com>
2024-03-20 16:57:34 -0500

    Main arreglado y parte de controlador

--- Laboratorio_Hotwheels/src/Control/ControladorColeccionGUI.java
+++ Laboratorio_Hotwheels/src/Control/ControladorColeccionGUI.java
@@ -9,15 +9,46 @@ import Modelo.Auto;
 
 class ControladorColeccionGUI {
     
+    //atributos
+    private ColeccionGUI unaColeccion;
+    private Auto unAuto;
     
-    
+        
     //Constructor
     public ControladorColeccionGUI() {
     }
 
-
+    //Métodos
     public void actionPerformed(ActionEvent e){
+        //eventos
+        //evento Agregar auto
+        
+        
+        //Evento Borrar Auto
+        
+        
+        //Evento Listar Autos
+        
+        
         
     }
+
+    // getters and setters
+    public ColeccionGUI getUnaColeccion() {
+        return unaColeccion;
+    }
+
+    public void setUnaColeccion(ColeccionGUI unaColeccion) {
+        this.unaColeccion = unaColeccion;
+    }
+
+    public Auto getUnAuto() {
+        return unAuto;
+    }
+
+    public void setUnAuto(Auto unAuto) {
+        this.unAuto = unAuto;
+    }
+    
     
 }
--- Laboratorio_Hotwheels/src/Main/Principal.java
+++ Laboratorio_Hotwheels/src/Main/Principal.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Main;
-import Control.ControladorColeccionGUI;
+import Control.ControladorAgregarAutoGUI;
 
 /**
  *
