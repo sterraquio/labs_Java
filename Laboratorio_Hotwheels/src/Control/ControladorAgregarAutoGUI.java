8d9748e97227a64ce91c3bc335a81c91cebb899e
camilo1210 <camilopavasrios@gmail.com>
2024-03-20 16:08:06 -0500

    ControladorAgregar
    controlador

--- Laboratorio_Hotwheels/src/Control/ControladorAgregarAutoGUI.java
+++ Laboratorio_Hotwheels/src/Control/ControladorAgregarAutoGUI.java
@@ -1,11 +1,26 @@
 package Control;
 
 
-public class ControladorAgregarAutoGUI {
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import Vista.AgregarAutoGUI;
+//import Modelo.Auto;
+
+
+public class ControladorAgregarAutoGUI implements ActionListener {
     
+    private AgregarAutoGUI vistaRegistro;
+//    private Auto carrito;
+    private ControladorColeccionGUI ctrlColeccion;
     
-    //constructor
-    public ControladorAgregarAutoGUI() {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
+    
+    
 }
+
