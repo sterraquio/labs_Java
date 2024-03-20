198da75292ea1e295dc1adb17f1a7110b9dfabf8
Andrick Buitrago <sterraquio@hotmail.com>
2024-03-20 16:14:45 -0500

    Agregue package a cada clase de modelo

--- Laboratorio_Hotwheels/src/Control/ControladorColeccionGUI.java
+++ Laboratorio_Hotwheels/src/Control/ControladorColeccionGUI.java
@@ -1,10 +1,23 @@
 package Control;
 
-class ControladorColeccionGUI {
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import Vista.AgregarAutoGUI;
+import Vista.ColeccionGUI;
+import Modelo.Auto;
+
+class ControladorColeccionGUI {
+    
+    
+    
     //Constructor
     public ControladorColeccionGUI() {
     }
 
+
+    public void actionPerformed(ActionEvent e){
+        
+    }
     
 }
