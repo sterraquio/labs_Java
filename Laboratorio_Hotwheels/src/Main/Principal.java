de0b8329191df9054856831610be922e4d97793c
camilo1210 <camilopavasrios@gmail.com>
2024-03-20 16:53:16 -0500

    main
    principal

---
+++ Laboratorio_Hotwheels/src/Main/Principal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+import Control.ControladorColeccionGUI;
+
+/**
+ *
+ * @author Camilo
+ */
+public class Principal {
+    public static void main(String[] args) {
+        ControlColeccionGUI coleccion = new controlColeccionGUI;
+    }
+    
+}
