01ddbc3e39901ed6d3142912708318f39daf8319
Andrick Buitrago <sterraquio@hotmail.com>
2024-04-14 16:42:10 -0500

    sin try cathc

--- Laboratorio_Hotwheels/src/Control/ControladorAgregarAutoGUI.java
+++ Laboratorio_Hotwheels/src/Control/ControladorAgregarAutoGUI.java
@@ -1,6 +1,5 @@
 package Control;
 
-
 //.
 import Control.ControladorColeccionGUI;
 import java.awt.event.ActionEvent;
@@ -8,51 +7,62 @@ import java.awt.event.ActionListener;
 import Vista.AgregarAutoGUI;
 import Modelo.Auto;
 import Modelo.Coleccion;
-
-
+import javax.swing.JOptionPane;
 
 public class ControladorAgregarAutoGUI implements ActionListener {
-    
+
     private AgregarAutoGUI vistaAgregar;
     private Auto carrito;
     private ControladorColeccionGUI ctrlColeccion;
     private Coleccion unosAutos;
-    
-    
-    public ControladorAgregarAutoGUI(){
-        
+
+    public ControladorAgregarAutoGUI() {
+
         this.unosAutos = new Coleccion();
-        this.vistaAgregar= new AgregarAutoGUI();
+        this.vistaAgregar = new AgregarAutoGUI();
         this.vistaAgregar.setVisible(true);
-        
+
         this.vistaAgregar.jbt_AgregarAuto.addActionListener(this);
-    }    
-    
-    
+    }
+
     @Override
     //EVENTO BOTON REGISTRAR
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == this.vistaAgregar.jbt_AgregarAuto){
-            carrito = new Auto();
-            
-            
-            
-            carrito.setAnhoSerie(Integer.parseInt(this.vistaAgregar.jtf_anhoSerie.getText()));
-            carrito.setCantTotalSerie(Integer.parseInt(this.vistaAgregar.jtf_canSerie.getText()));
-            carrito.setnumeroSerie(Integer.parseInt(this.vistaAgregar.jtf_numSerie.getText()));
-            carrito.setMarca(this.vistaAgregar.jtf_marca.getText());
-            carrito.setColor(this.vistaAgregar.jtf_color.getText());
-            
-            
-            this.ctrlColeccion.getColeccionAutos().getAutos().add(carrito);
-           
-            
-            
-            //Para mostrar que se guardó correctamente
-            javax.swing.JOptionPane.showMessageDialog(vistaAgregar, "La auto del año : "+this.vistaAgregar.jtf_anhoSerie.getText()+
-                                                                       "\nCon el numero de serie : "+this.vistaAgregar.jtf_numSerie.getText()+
-                                                                        "\nFue agregado con Exito");
-            this.vistaAgregar.dispose();
+        if (e.getSource() == this.vistaAgregar.jbt_AgregarAuto) {
+            while (true) {
+                try {
+                    carrito = new Auto();
+                    carrito.setAnhoSerie(Integer.parseInt(this.vistaAgregar.jtf_anhoSerie.getText()));
+                    carrito.setCantTotalSerie(Integer.parseInt(this.vistaAgregar.jtf_canSerie.getText()));
+                    carrito.setnumeroSerie(Integer.parseInt(this.vistaAgregar.jtf_numSerie.getText()));
+                    carrito.setMarca(this.vistaAgregar.jtf_marca.getText());
+                    carrito.setColor(this.vistaAgregar.jtf_color.getText());
+
+                    if (Integer.parseInt(this.vistaAgregar.jtf_anhoSerie.getText()) >= 1000) {
+                        if (Integer.parseInt(this.vistaAgregar.jtf_numSerie.getText()) <= Integer.parseInt(this.vistaAgregar.jtf_canSerie.getText()) && Integer.parseInt(this.vistaAgregar.jtf_numSerie.getText()) > 0) {
+                            //Agrega a la coleccion
+                            this.ctrlColeccion.getColeccionAutos().getAutos().add(carrito);
+                            //Para mostrar que se guardó correctamente
+                            javax.swing.JOptionPane.showMessageDialog(vistaAgregar, "La auto del año : " + this.vistaAgregar.jtf_anhoSerie.getText()
+                                    + "\nCon el numero de serie : " + this.vistaAgregar.jtf_numSerie.getText()
+                                    + "\nFue agregado con Exito");
+                            this.vistaAgregar.dispose();
+                        } else {
+                            JOptionPane.showMessageDialog(null, "EROR, el número de serie debe ser igual o menor que la cantidad de la serie y mayor que 0");
+                            this.vistaAgregar.jtf_numSerie.grabFocus();
+                        }
+                    } else {
+                        JOptionPane.showMessageDialog(null, "El año de la serie debe ser mayor o igual que 4 digitos");
+                        this.vistaAgregar.jtf_anhoSerie.setText("");
+                        this.vistaAgregar.jtf_anhoSerie.grabFocus();
+                    }
+
+                    break;
+                } catch (NumberFormatException error_1) {
+                    JOptionPane.showMessageDialog(null, "Error, con el año de serie o numero de serie o cantidad de serie. DEBEN SER VALORES NUMERICOS, además todos los recuadros deben estar llenos");
+                    break;
+                }
+            }
         }
     }
 
@@ -82,6 +92,4 @@ public class ControladorAgregarAutoGUI implements ActionListener {
         this.vistaAgregar.jtf_anhoSerie.setText(this.ctrlColeccion.getUnaColeccion().jtf_anhoSerieC.getText());
     }
 
-    
 }
-
